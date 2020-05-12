f31e0d1c154b647358dbb72d3e784d7e575dad77
Gurjeet Singh <gurjeet@singh.im>
2020-05-12 12:54:11 -0700

    Added DNS A records for root and www subdomain
    Removed the TXT records that were added for verification, since Gitlab
    has successfully finished verification.

--- infrastructure/dns.tf
+++ infrastructure/dns.tf
@@ -22,22 +22,9 @@ variable "cf_zone_id" {
 
 variable "all_records" {
   default = [
-    ["A",    "*",    "46.166.184.99",              false],
-    ["A",    "*",    "185.206.180.117",            false],
-    ["A",    "q.ht", "46.166.184.99",              true ],
-    ["A",    "q.ht", "185.206.180.117",            true ],
-    ["A",    "www",  "46.166.184.99",              true ],
-    ["A",    "www",  "185.206.180.117",            true ],
-    ["AAAA", "*",    "2a00:1768:2001:63::46:99",   false],
-    ["AAAA", "*",    "2a0b:1640:1:1:1:1:bb7:e646", false],
-    ["AAAA", "q.ht", "2a00:1768:2001:63::46:99",   true ],
-    ["AAAA", "q.ht", "2a0b:1640:1:1:1:1:bb7:e646", true ],
-    ["AAAA", "www",  "2a00:1768:2001:63::46:99",   true ],
-    ["AAAA", "www",  "2a0b:1640:1:1:1:1:bb7:e646", true ],
     # Record type, record name, IP Address/value, is-proxied?
-
-    ["TXT", "_gitlab-pages-verification-code.q.ht", "gitlab-pages-verification-code=a0672e704d12da7b1ca76e71fed79f68", false],
-    ["TXT", "_gitlab-pages-verification-code.www.q.ht", "gitlab-pages-verification-code=e0df8376078e36d180aa06e3de58d3b4", false],
+    ["A",    "@",   "35.185.44.232", true],
+    ["A",    "www", "35.185.44.232", true ],
   ]
 }
 
